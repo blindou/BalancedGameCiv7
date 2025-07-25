c2a5a3d4024d97305096b7c50176fdce621c72dc
farail baptiste <farailba@gmail.com>
2025-07-25 11:38:21 +0200

    add new timer

--- Data/Sql/Exploration/Global.sql
+++ Data/Sql/Exploration/Global.sql
@@ -7,3 +7,9 @@ WHERE DiplomacyActionType = 'DIPLOMACY_ACTION_ESPIONAGE_STEAL_TECH';
 UPDATE DiplomaticActionInfluenceCosts
 SET	InfCostHostile = 240, InfCostUnfriendly = 240, InfCostNeutral = 240, InfCostFriendly = 240, InfCostHelpful = 240
 WHERE DiplomacyActionType = 'DIPLOMACY_ACTION_ESPIONAGE_STEAL_CIVIC';
+
+-- Timer
+
+UPDATE TurnSegments
+SET TimeLimit_Base = 45
+WHERE TurnSegmentType = 'TURNSEG_SINGLEPHASE_MBG';
--- Data/Sql/Modern/Global.sql
+++ Data/Sql/Modern/Global.sql
@@ -7,3 +7,9 @@ WHERE DiplomacyActionType = 'DIPLOMACY_ACTION_ESPIONAGE_STEAL_TECH';
 UPDATE DiplomaticActionInfluenceCosts
 SET	InfCostHostile = 320, InfCostUnfriendly = 320, InfCostNeutral = 320, InfCostFriendly = 320, InfCostHelpful = 320
 WHERE DiplomacyActionType = 'DIPLOMACY_ACTION_ESPIONAGE_STEAL_CIVIC';
+
+-- Timer
+
+UPDATE TurnSegments
+SET TimeLimit_Base = 30
+WHERE TurnSegmentType = 'TURNSEG_SINGLEPHASE_MBG';
