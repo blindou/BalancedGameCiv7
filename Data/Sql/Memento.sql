22a532e998d290b62fd4fad2d7215b25c7ae7fc5
farail baptiste <farailba@gmail.com>
2025-07-27 01:29:45 +0200

    add warclub to 05

--- Data/Sql/Memento.sql
+++ Data/Sql/Memento.sql
@@ -3,3 +3,8 @@ UPDATE ModifierArguments
 SET	Value = 15
 WHERE ModifierId = 'MEMENTO_BENJAMIN_FRANKLIN_BIFOCALS_MODIFIER' and name = 'Amount';
 
+-- WarClub
+
+UPDATE ModifierArguments
+SET Value = 0.5
+WHERE ModifierId IN ('MEMENTO_TECUMSEH_WARCLUB_MODIFIER') and name = 'Amount';
