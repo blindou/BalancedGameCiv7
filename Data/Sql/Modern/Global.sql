04bb713dbdc4881c86ae913a568fada52254a5ff
farail baptiste <farailba@gmail.com>
2025-07-27 15:31:31 +0200

    Change chrono

--- Data/Sql/Modern/Global.sql
+++ Data/Sql/Modern/Global.sql
@@ -11,5 +11,5 @@ WHERE DiplomacyActionType = 'DIPLOMACY_ACTION_ESPIONAGE_STEAL_CIVIC';
 -- Timer
 
 UPDATE TurnSegments
-SET TimeLimit_Base = 1
+SET TimeLimit_Base = 30
 WHERE TurnSegmentType = 'TURNSEG_SINGLEPHASE_MBG';
