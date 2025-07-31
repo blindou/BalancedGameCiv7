14eec159527dc53184e0eb5068194eeb01a911cc
farail baptiste <farailba@gmail.com>
2025-07-31 15:40:50 +0200

    Victory railroad 500 to 300

---
+++ Data/Sql/Modern/Victories.sql
@@ -0,0 +1,13 @@
+-- Eco
+
+UPDATE AgeProgressionMilestones
+SET RequiredPathPoints = 100
+where AgeProgressionMilestoneType = 'MODERN_ECONOMIC_MILESTONE_1';
+
+UPDATE AgeProgressionMilestones
+SET RequiredPathPoints = 200
+where AgeProgressionMilestoneType = 'MODERN_ECONOMIC_MILESTONE_2';
+
+UPDATE AgeProgressionMilestones
+SET RequiredPathPoints = 300
+where AgeProgressionMilestoneType = 'MODERN_ECONOMIC_MILESTONE_3';
